22433e11815e82f8b712ce00d3d4370f766daa59
ZedMurphy <Spreading.Chaoz@gmail.com>
2025-03-21 18:42:27 +0100

    it's moving!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -12,12 +13,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))  # Call fill on the screen object
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        time = clock.tick(60)
+        dt = time/1000
 
 
 
